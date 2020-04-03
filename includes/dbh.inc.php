f07718afa3544a304f31b1f807f288f309d564f8
samNewDev <62222050+samNewDev@users.noreply.github.com>
2020-04-03 12:03:27 +0200

    Update dbh.inc.php

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -2,7 +2,7 @@
   $servername = "localhost";
   $dbUsername = "root";
   $dbPassword = "";
-  $dbName = "silver technology";
+  $dbName = "silverTechnology";
  //run a connection
  $conn = mysqli_connect($servername, $dbUsername, $dbPassword, $dbName);
 
